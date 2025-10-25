90a189196490f1e56eb2ff06b860dbc458e2db4e
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-10-25 16:22:25 +0300

    Example Client file

--- Client/Client.py
+++ Client/Client.py
@@ -1,1 +1,9 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.sendto("Hello".encode(), ('127.0.0.1', 9999))
+
+data, addr = client.recvfrom(1024)
+print(data.decode())
+
 
