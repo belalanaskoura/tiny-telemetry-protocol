bbb851bd967d6cbcfed7be52825be255a948a2e2
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-10-25 16:23:51 +0300

    Example Server file

--- Server/Server.py
+++ Server/Server.py
@@ -1,1 +1,10 @@
+import socket
 
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind(('127.0.0.1', 9999))
+
+
+while True:
+    data,addr = server.recvfrom(1024)
+    print(data.decode())
+    server.sendto("Hi".encode(),addr)
