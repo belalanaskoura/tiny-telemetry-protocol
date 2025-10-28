1c63e769277be46571d5de01c56ad8c8eeea8ac0
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-10-28 20:44:37 +0300

    Client file is updated
    1-Port number is now updated to match server
    2- Added Duration variable to specify how long the program runs before termination
    3- Matched Header changes in server

--- Client/Client.py
+++ Client/Client.py
@@ -4,37 +4,44 @@ import time
 import random
 
 # Header format same as server
-HEADER_FORMAT = "!HHIBBH"
+HEADER_FORMAT = "!HBBIBHB"
 MSG_INIT = 1
 MSG_DATA = 2
 MSG_HEARTBEAT = 3
 
 SERVER_IP = "127.0.0.1"
-SERVER_PORT = 5005
-DEVICE_ID = 1
-INTERVAL = 1 
+SERVER_PORT = 9999
+DEVICE_ID = 1 #Changes based on the number of sensors
+INTERVAL = 1
+DURATION = 60 #Sends 60 packets per minute 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 seq_num = 0
 
-def build_header(device_id, seq_num, msg_type, flags=0):
+# Function to create header of packet
+def build_header(device_id, seq_num, msg_type, batch_flag = 0, checksum = 0, version = 1):
     timestamp = int(time.time())
-    return struct.pack(HEADER_FORMAT, device_id, seq_num, timestamp, msg_type, flags, 0)
+    return struct.pack(HEADER_FORMAT, device_id, seq_num, timestamp, msg_type, batch_flag, checksum, version)
 
-# Step 1: Send INIT packet
-init_header = build_header(DEVICE_ID, seq_num, MSG_INIT)
+# Send INIT packet
+init_header = build_header(DEVICE_ID, seq_num, MSG_INIT, version= 1)
 client_socket.sendto(init_header, (SERVER_IP, SERVER_PORT))
 print("[INIT] sent to server")
 
-# Step 2: Periodically send data packets
-while True:
+#Send data packets for the specified duration
+start_time = time.time()
+
+while time.time() - start_time < DURATION:
     seq_num += 1
-    header = build_header(DEVICE_ID, seq_num, MSG_DATA)
+    header = build_header(DEVICE_ID, seq_num, MSG_DATA, batch_flag= 0, checksum= 0, version= 1)
     # Simulated sensor reading (temperature)
-    temperature = round(random.uniform(20.0, 21.5), 2)
+    temperature = round(random.uniform(20, 34), 1)
     payload = f"{temperature}".encode("utf-8")
     packet = header + payload
 
     client_socket.sendto(packet, (SERVER_IP, SERVER_PORT))
     print(f"[DATA] sent seq={seq_num}, temp={temperature}")
     time.sleep(INTERVAL)
+
+print(f" Finished sending data for {DURATION} seconds.")
+client_socket.close()    
