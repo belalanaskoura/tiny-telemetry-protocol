b5a4faa4ab224588e711a042d0fe00df5fa1c030
belalanaskoura <153070932+belalanaskoura@users.noreply.github.com>
2025-10-28 22:33:35 +0300

    Minor client change

--- Client/Client.py
+++ Client/Client.py
@@ -11,21 +11,21 @@ MSG_HEARTBEAT = 3
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9999
-DEVICE_ID = 1 #Changes based on the number of sensors
+DEVICE_ID = 1 # Changes based on the number of sensors
 INTERVAL = 1
-DURATION = 60 #Sends 60 packets per minute 
+DURATION = 60 # Sends 60 packets per minute 
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 seq_num = 0
 
 # Function to create header of packet
-def build_header(device_id, seq_num, msg_type, batch_flag=0, checksum=0, version=1):
+def construct_header(device_id, seq_num, msg_type, batch_flag=0, checksum=0, version=1):
     timestamp = int(time.time())
     return struct.pack(HEADER_FORMAT, seq_num, device_id, msg_type, timestamp, batch_flag, checksum, version)
 
 
 # Send INIT packet
-init_header = build_header(DEVICE_ID, seq_num, MSG_INIT, version= 1)
+init_header = construct_header(DEVICE_ID, seq_num, MSG_INIT, version= 1)
 client_socket.sendto(init_header, (SERVER_IP, SERVER_PORT))
 print("[INIT] sent to server")
 
@@ -34,7 +34,7 @@ start_time = time.time()
 #Send data packets for the specified duration
 while time.time() - start_time < DURATION:
     seq_num += 1
-    header = build_header(DEVICE_ID, seq_num, MSG_DATA, batch_flag= 0, checksum= 0, version= 1)
+    header = construct_header(DEVICE_ID, seq_num, MSG_DATA, batch_flag= 0, checksum= 0, version= 1)
 
     # Simulated sensor reading (temperature)
     temperature = round(random.uniform(20.0, 34.0), 1)
